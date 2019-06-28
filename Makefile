a83c16094b2a4d029dc79fd68459cc0df516791b
Steve Marshall <steve@stevemarshall.com>
2019-06-28 16:47:06 +0100

    Generate config for all known defensive domains

--- Makefile
+++ Makefile
@@ -1,11 +1,59 @@
-all: $(DEFENSIVE_DOMAINS:%=defensive-domains/config/%.yaml)
+include defensive-domains/domains.mk
+
+all:
+.PHONY: all
 
 install:
 	brew install octodns
+.PHONY: install
+
+
+# Defensive domain actions
+
+defensive-domains-validate: defensive-domains/config.yaml \
+	$(DEFENSIVE_DOMAINS:%=defensive-domains/config/%.yaml)
+	octodns-validate --config-file=defensive-domains/config.yaml
+.PHONY: defensive-domains-validate
+
+defensive-domains-noop: defensive-domains/config.yaml \
+	$(DEFENSIVE_DOMAINS:%=defensive-domains/config/%.yaml) \
+	defensive-domains-validate
+	octodns-sync --config-file=defensive-domains/config.yaml
+.PHONY: defensive-domains-noop
+
+defensive-domains-apply: defensive-domains/config.yaml \
+	$(DEFENSIVE_DOMAINS:%=defensive-domains/config/%.yaml) \
+	defensive-domains-validate
+	octodns-sync --config-file=defensive-domains/config.yaml --doit
+.PHONY: defensive-domains-noop
+
+defensive-domains-get-live-config: \
+	$(DEFENSIVE_DOMAINS:%=defensive-domains/live/%.yaml)
+.PHONY: get-live-defensive-domain-config
+
+
+# Defensive domain config
+
+defensive-domains/config.yaml: defensive-domains/config.tmpl.yaml defensive-domains/domains.mk
+	@cat defensive-domains/config.tmpl.yaml > defensive-domains/config.yaml
+	@echo "  $(DEFENSIVE_DOMAINS:%=%.:\n    sources:\n      - config\n    targets:\n      - route53\n )" >> defensive-domains/config.yaml
 
 defensive-domains/config/%.yaml: defensive-domains/dns.tmpl.yaml
 	@mkdir -p defensive-domains/config/
 	@ln -s ../$(<F) $@
 
+defensive-domains/live/%.yaml: defensive-domains/config.yaml
+	octodns-dump \
+	  --config-file=defensive-domains/config.yaml \
+	  --output-dir=defensive-domains/live/ \
+	  $*. route53
+
+
+# Cleanup
+
 clean:
-	@rm -rf defensive-domains/config/*.yaml
+	@rm -rf \
+	  defensive-domains/config.yaml \
+	  defensive-domains/config/ \
+	  defensive-domains/live/
+.PHONY: clean
---
+++ defensive-domains/domains.mk
@@ -0,0 +1,417 @@
+# moj
+DEFENSIVE_DOMAINS= \
+  moj.org.uk \
+  moj.uk
+
+# justice
+DEFENSIVE_DOMAINS+= \
+  justice.uk
+
+# justicegov
+DEFENSIVE_DOMAINS+= \
+  justicegov.uk
+
+# ministryofjustice
+DEFENSIVE_DOMAINS+= \
+  ministryofjustice.uk
+
+# ministry-of-justice
+DEFENSIVE_DOMAINS+= \
+  ministry-of-justice.com \
+  ministry-of-justice.org.uk \
+  ministry-of-justice.org \
+  ministry-of-justice.net \
+  ministry-of-justice.uk
+
+# justiceministry
+DEFENSIVE_DOMAINS+= \
+  justiceministry.co.uk \
+  justiceministry.org.uk \
+  justiceministry.org \
+  justiceministry.net \
+  justiceministry.uk
+
+# justice-ministry
+DEFENSIVE_DOMAINS+= \
+  justice-ministry.com \
+  justice-ministry.co.uk \
+  justice-ministry.org.uk \
+  justice-ministry.org \
+  justice-ministry.net \
+  justice-ministry.uk
+
+# ministryjustice
+DEFENSIVE_DOMAINS+= \
+  ministryjustice.com \
+  ministryjustice.co.uk \
+  ministryjustice.org.uk \
+  ministryjustice.org \
+  ministryjustice.net \
+  ministryjustice.uk
+
+# ministry-justice
+DEFENSIVE_DOMAINS+= \
+  ministry-justice.com \
+  ministry-justice.co.uk \
+  ministry-justice.org.uk \
+  ministry-justice.org \
+  ministry-justice.net \
+  ministry-justice.uk
+
+# hmcts
+DEFENSIVE_DOMAINS+= \
+  hmcts.uk
+
+# hmcs
+DEFENSIVE_DOMAINS+= \
+  hmcs.org.uk \
+  hmcs.uk
+
+# court
+DEFENSIVE_DOMAINS+= \
+  court.uk
+
+# courts
+DEFENSIVE_DOMAINS+= \
+  courts.org.uk \
+  courts.uk
+
+# courtservice
+DEFENSIVE_DOMAINS+= \
+  courtservice.org \
+  courtservice.net \
+  courtservice.uk
+
+# courtsservice
+DEFENSIVE_DOMAINS+= \
+  courtsservice.co.uk \
+  courtsservice.org.uk \
+  courtsservice.uk
+
+# court-service
+DEFENSIVE_DOMAINS+= \
+  court-service.org-uk \
+  court-service.org \
+  court-service.net \
+  court-service.uk
+
+# courts-service
+DEFENSIVE_DOMAINS+= \
+  courts-service.com \
+  courts-service.co.uk \
+  courts-service.org.uk \
+  courts-service.org \
+  courts-service.net \
+  courts-service.uk
+
+# tribunal
+DEFENSIVE_DOMAINS+= \
+  tribunal.org.uk \
+  tribunal.uk
+
+# tribunals
+DEFENSIVE_DOMAINS+= \
+  tribunals.uk
+
+# tribunalservice
+DEFENSIVE_DOMAINS+= \
+  tribunalservice.co.uk \
+  tribunalservice.org.uk \
+  tribunalservice.org \
+  tribunalservice.net \
+  tribunalservice.uk
+
+# tribunalsservice
+DEFENSIVE_DOMAINS+= \
+  tribunalsservice.com \
+  tribunalsservice.co.uk \
+  tribunalsservice.org.uk \
+  tribunalsservice.org \
+  tribunalsservice.net \
+  tribunalsservice.uk
+
+# tribunal-service
+DEFENSIVE_DOMAINS+= \
+  tribunal-service.com \
+  tribunal-service.org \
+  tribunal-service.net \
+  tribunal-service.uk
+
+# tribunals-service
+DEFENSIVE_DOMAINS+= \
+  tribunals-service.com \
+  tribunals-service.co.uk \
+  tribunals-service.org.uk \
+  tribunals-service.org \
+  tribunals-service.net \
+  tribunals-service.uk
+
+# hermajestyscourtservice
+DEFENSIVE_DOMAINS+= \
+  hermajestyscourtservice.com \
+  hermajestyscourtservice.co.uk \
+  hermajestyscourtservice.org.uk \
+  hermajestyscourtservice.org \
+  hermajestyscourtservice.net \
+  hermajestyscourtservice.uk
+
+# her-majestys-court-service
+DEFENSIVE_DOMAINS+= \
+  her-majestys-court-service.com \
+  her-majestys-court-service.co.uk \
+  her-majestys-court-service.org.uk \
+  her-majestys-court-service.org \
+  her-majestys-court-service.net \
+  her-majestys-court-service.uk
+
+# hermajestyscourtsservice
+DEFENSIVE_DOMAINS+= \
+  hermajestyscourtsservice.com \
+  hermajestyscourtsservice.co.uk \
+  hermajestyscourtsservice.org.uk \
+  hermajestyscourtsservice.org \
+  hermajestyscourtsservice.net \
+  hermajestyscourtsservice.uk
+
+# her-majestys-courts-service
+DEFENSIVE_DOMAINS+= \
+  her-majestys-courts-service.com \
+  her-majestys-courts-service.co.uk \
+  her-majestys-courts-service.org.uk \
+  her-majestys-courts-service.org \
+  her-majestys-courts-service.net \
+  her-majestys-courts-service.uk
+
+# noms
+DEFENSIVE_DOMAINS+= \
+  noms.org.uk \
+  noms.uk
+
+# nationaloffendersmanagementservice
+DEFENSIVE_DOMAINS+= \
+  nationaloffendersmanagementservice.com \
+  nationaloffendersmanagementservice.co.uk \
+  nationaloffendersmanagementservice.org.uk \
+  nationaloffendersmanagementservice.org \
+  nationaloffendersmanagementservice.net \
+  nationaloffendersmanagementservice.uk
+
+# national-offenders-management-service
+DEFENSIVE_DOMAINS+= \
+  national-offenders-management-service.com \
+  national-offenders-management-service.co.uk \
+  national-offenders-management-service.org.uk \
+  national-offenders-management-service.org \
+  national-offenders-management-service.net \
+  national-offenders-management-service.uk
+
+# hmpps
+DEFENSIVE_DOMAINS+= \
+  hmpps.org.uk \
+  hmpps.org \
+  hmpps.uk
+
+# prisons
+DEFENSIVE_DOMAINS+= \
+  prisons.uk
+
+# prison
+DEFENSIVE_DOMAINS+= \
+  prison.uk
+
+# prisonservice
+DEFENSIVE_DOMAINS+= \
+  prisonservice.org.uk \
+  prisonservice.org \
+  prisonservice.uk
+
+# prison-service
+DEFENSIVE_DOMAINS+= \
+  prison-service.org.uk \
+  prison-service.org \
+  prison-service.uk
+
+# prisonsservice
+DEFENSIVE_DOMAINS+= \
+  prisonsservice.com \
+  prisonsservice.co.uk \
+  prisonsservice.org.uk \
+  prisonsservice.org \
+  prisonsservice.net \
+  prisonsservice.uk
+
+# prisons-service
+DEFENSIVE_DOMAINS+= \
+  prisons-service.com \
+  prisons-service.co.uk \
+  prisons-service.org.uk \
+  prisons-service.org \
+  prisons-service.net \
+  prisons-service.uk
+
+# probation
+DEFENSIVE_DOMAINS+= \
+  probation.uk
+
+# probationservice
+DEFENSIVE_DOMAINS+= \
+  probationservice.org.uk \
+  probationservice.org \
+  probationservice.uk
+
+# probation-service
+DEFENSIVE_DOMAINS+= \
+  probation-service.com \
+  probation-service.co.uk \
+  probation-service.org.uk \
+  probation-service.org \
+  probation-service.net \
+  probation-service.uk
+
+# hermajestysprisonandprobationservice
+DEFENSIVE_DOMAINS+= \
+  hermajestysprisonandprobationservice.com \
+  hermajestysprisonandprobationservice.co.uk \
+  hermajestysprisonandprobationservice.org.uk \
+  hermajestysprisonandprobationservice.org \
+  hermajestysprisonandprobationservice.net \
+  hermajestysprisonandprobationservice.uk
+
+# her-majestys-prison-and-probation-service
+DEFENSIVE_DOMAINS+= \
+  her-majestys-prison-and-probation-service.com \
+  her-majestys-prison-and-probation-service.co.uk \
+  her-majestys-prison-and-probation-service.org.uk \
+  her-majestys-prison-and-probation-service.org \
+  her-majestys-prison-and-probation-service.net \
+  her-majestys-prison-and-probation-service.uk
+
+# hermajestysprisonservice
+DEFENSIVE_DOMAINS+= \
+  hermajestysprisonservice.com \
+  hermajestysprisonservice.co.uk \
+  hermajestysprisonservice.org.uk \
+  hermajestysprisonservice.org \
+  hermajestysprisonservice.net \
+  hermajestysprisonservice.uk
+
+# her-majestys-prison-service
+DEFENSIVE_DOMAINS+= \
+  her-majestys-prison-service.com \
+  her-majestys-prison-service.co.uk \
+  her-majestys-prison-service.org.uk \
+  her-majestys-prison-service.org \
+  her-majestys-prison-service.net \
+  her-majestys-prison-service.uk
+
+# hermajestysprisonsservice
+DEFENSIVE_DOMAINS+= \
+  hermajestysprisonsservice.com \
+  hermajestysprisonsservice.co.uk \
+  hermajestysprisonsservice.org.uk \
+  hermajestysprisonsservice.org \
+  hermajestysprisonsservice.net \
+  hermajestysprisonsservice.uk
+
+# her-majestys-prisons-service
+DEFENSIVE_DOMAINS+= \
+  her-majestys-prisons-service.com \
+  her-majestys-prisons-service.co.uk \
+  her-majestys-prisons-service.org.uk \
+  her-majestys-prisons-service.org \
+  her-majestys-prisons-service.net \
+  her-majestys-prisons-service.uk
+
+# laa
+DEFENSIVE_DOMAINS+= \
+  laa.uk
+
+# legalaid
+DEFENSIVE_DOMAINS+= \
+  legalaid.uk
+
+# legal-aid
+DEFENSIVE_DOMAINS+= \
+  legal-aid.org.uk \
+  legal-aid.uk
+
+# legalaidagency
+DEFENSIVE_DOMAINS+= \
+  legalaidagency.net \
+  legalaidagency.uk
+
+# legal-aid-agency
+DEFENSIVE_DOMAINS+= \
+  legal-aid-agency.com \
+  legal-aid-agency.co.uk \
+  legal-aid-agency.org.uk \
+  legal-aid-agency.org \
+  legal-aid-agency.net \
+  legal-aid-agency.uk
+
+# opg
+DEFENSIVE_DOMAINS+= \
+  opg.uk
+
+# publicguardian
+DEFENSIVE_DOMAINS+= \
+  publicguardian.org.uk \
+  publicguardian.uk
+
+# public-guardian
+DEFENSIVE_DOMAINS+= \
+  public-guardian.com \
+  public-guardian.co.uk \
+  public-guardian.org.uk \
+  public-guardian.org \
+  public-guardian.net \
+  public-guardian.uk
+
+# officeofthepublicguardian
+DEFENSIVE_DOMAINS+= \
+  officeofthepublicguardian.com \
+  officeofthepublicguardian.co.uk \
+  officeofthepublicguardian.org.uk \
+  officeofthepublicguardian.org \
+  officeofthepublicguardian.net \
+  officeofthepublicguardian.uk
+
+# office-of-the-public-guardian
+DEFENSIVE_DOMAINS+= \
+  office-of-the-public-guardian.com \
+  office-of-the-public-guardian.co.uk \
+  office-of-the-public-guardian.org.uk \
+  office-of-the-public-guardian.org \
+  office-of-the-public-guardian.net \
+  office-of-the-public-guardian.uk
+
+# yjb
+DEFENSIVE_DOMAINS+= \
+  yjb.uk
+
+# youthjustice
+DEFENSIVE_DOMAINS+= \
+  youthjustice.net \
+  youthjustice.uk
+
+# youth-justice
+DEFENSIVE_DOMAINS+= \
+  youth-justice.uk
+
+# youthjusticeboard
+DEFENSIVE_DOMAINS+= \
+  youthjusticeboard.com \
+  youthjusticeboard.co.uk \
+  youthjusticeboard.org.uk \
+  youthjusticeboard.org \
+  youthjusticeboard.net \
+  youthjusticeboard.uk
+
+# youth-justice-board
+DEFENSIVE_DOMAINS+= \
+  youth-justice-board.com \
+  youth-justice-board.co.uk \
+  youth-justice-board.org.uk \
+  youth-justice-board.org \
+  youth-justice-board.net \
+  youth-justice-board.uk
