bb93a05d61fffcd619afea36f2525da4b16dcbc4
Steve Marshall <steve@stevemarshall.com>
2019-12-20 17:29:54 +0000

    Check if we already own a domain we're registering
    If we already own a domain, this script says it's "not available" which,
    while true, isn't the most useful or accurate response it could offer.
    
    Instead, we can quickly check to see if it's something we already own
    and, if so, skip the other (slower) checks.
    
    I've run all of the domains in `defensive-domains/domains.mk` through
    this, and it identified (confirming @AntonyBishop's suggestion) that
    we didn't own the following (but could):
    
        her-majestys-courts-service.uk
        her-majestys-prison-and-probation-service.uk
        legalaidagency.uk
        national-offenders-management-service.uk
        prisonservice.uk
        public-guardian.uk
        publicguardian.uk
    
    And that we don't, and can't, own:
    
        court.uk
        courts.uk
        courtservice.uk
        hermajestyscourtservice.uk
        hmcs.uk
        hmcts.uk
        laa.uk
        legal-aid.uk
        legalaid.uk
        ministryofjustice.uk
        moj.uk
        noms.uk
        prison.uk
        prisons.uk
        probation.uk
        sentencingcouncil.uk
        tribunal.uk
        tribunals.uk
        yjb.uk
        youth-justice.uk
        youthjustice.uk
    
    In a subsequent commit, I'll remove all of those from
    `defensive-domains/domains.mk`.

--- script/register-domains.sh
+++ script/register-domains.sh
@@ -3,7 +3,14 @@
 domains=("$@")
 registrant="FirstName=Steve,LastName=Marshall,ContactType=PUBLIC_BODY,OrganizationName=Ministry of Justice,AddressLine1=102 Petty France,City=London,CountryCode=GB,ZipCode=SW1H 9AJ,Email=domains@digital.justice.gov.uk,PhoneNumber=+44.2033343555,ExtraParams=[{Name=UK_CONTACT_TYPE,Value=GOV}]"
 
+existing_domains=$(aws route53domains list-domains --region us-east-1 | jq -r '.Domains[] .DomainName')
+
 for domain in "${domains[@]}"; do
+  if [[ ${existing_domains[*]} =~ ${domain} ]]; then
+    echo "$domain is already registered"
+    continue
+  fi
+
   availability=$(aws route53domains check-domain-availability \
     --domain-name "$domain" --region us-east-1 \
     | jq -r '.Availability')
