363b48394c7212c784cb699f0d8ea3a351d5c88d
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-06-28 19:21:06 +0100

    :white_check_mark: Correct broken facade tests

--- tests/test_route53_facade.py
+++ tests/test_route53_facade.py
@@ -12,11 +12,36 @@ class TestRoute53Facade(unittest.TestCase):
 
     def test_get_aws_zones_successfully(self):
         self.mock_boto3.client().get_paginator().paginate.return_value = [
-            {"HostedZones": [{"Id": "zone_id", "Name": "example.com."}]}
+            {"HostedZones": [{"Id": "zone_id", "Name": "example.com."}]},
         ]
         zones = self.route53.get_aws_zones()
 
         self.assertEqual(zones, {("zone_id", "example.com")})
+
+    def test_is_zone_empty_is_recognised(self):
+        self.mock_boto3.client().get_paginator().paginate.return_value = [
+            {
+                "ResourceRecordSets": [
+                    {"Type": "NS"},
+                    {"Type": "SOA"},
+                ]
+            },
+        ]
+        is_empty = self.route53.is_zone_empty("zone_id")
+        self.assertTrue(is_empty)
+
+    def test_non_empty_zone_isnt_recognised(self):
+        self.mock_boto3.client().get_paginator().paginate.return_value = [
+            {
+                "ResourceRecordSets": [
+                    {"Type": "NS"},
+                    {"Type": "SOA"},
+                    {"Type": "A"},
+                ]
+            },
+        ]
+        is_empty = self.route53.is_zone_empty("zone_id")
+        self.assertFalse(is_empty)
 
 
 if __name__ == "__main__":
