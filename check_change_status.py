8b6532708257392252670aa7b28b6a5978468d83
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2024-08-30 15:32:31 +0100

    test run check change on github env

--- check_change_status.py
+++ check_change_status.py
@@ -3,9 +3,15 @@ import sys
 
 from providers.route53 import Route53Service
 
+env_file = os.getenv("GITHUB_ENV")
+
 def main():
-    service = Route53Service()
-    print(service.get_aws_zones())
+    # service = Route53Service()
+    # print(service.get_aws_zones())
+    with open(env_file, "a", encoding="utf-8") as f:
+        lines = f.readlines()
+    for line in lines:
+        print(line)
 
 
 if __name__ == "__main__":
