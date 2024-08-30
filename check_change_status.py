7694769aefb421164ad28a105b30de65d29ba07e
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2024-08-30 16:26:59 +0100

    deal with yml

--- check_change_status.py
+++ check_change_status.py
@@ -5,13 +5,22 @@ from providers.route53 import Route53Service
 
 hosted_zone_changed_files = os.getenv("hosted_zone_changed_files")
 
-hosted_zone_names = [name[12:-5] for name in hosted_zone_changed_files.split(" ")]
+# Strip hostedzones/ from prefix and .yaml from suffix
+# hosted_zone_names = [path[12:-5] for path in hosted_zone_changed_files.split(" ")]
+
+hosted_zone_names = []
+for path in hosted_zone_changed_files:
+    if path[-5:] == ".yaml":
+        hosted_zone_names.append(path[12:-5])
+    elif path[-4:] == ".yml":
+        hosted_zone_names.append(path[12:-4])
+    else:
+        print("not a yml or a yaml!!!")
+
 
 def main():
     # # service = Route53Service()
     # # print(service.get_aws_zones())
-    # with open(env_file, "r", encoding="utf-8") as f:
-    #     content = f.readlines()
 
     print(hosted_zone_changed_files)
     print(hosted_zone_names)
