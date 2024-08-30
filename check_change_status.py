7ba170c0af5d2962b92a1867494fedb8ab8dd08a
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2024-08-30 15:46:27 +0100

    ref correct object name

--- check_change_status.py
+++ check_change_status.py
@@ -9,7 +9,7 @@ def main():
     # service = Route53Service()
     # print(service.get_aws_zones())
     with open(env_file, "a", encoding="utf-8") as f:
-        lines = [line.rstrip() for line in file]
+        lines = [line.rstrip() for line in f]
     
     print(lines)
 
