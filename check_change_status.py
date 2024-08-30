a92cd0690b29200ae404ad645e82d6dcf732bf12
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2024-08-30 16:19:14 +0100

    split hosted zone file names to names

--- check_change_status.py
+++ check_change_status.py
@@ -5,6 +5,8 @@ from providers.route53 import Route53Service
 
 hosted_zone_changed_files = os.getenv("hosted_zone_changed_files")
 
+hosted_zone_names = [name[12:-5] for name in hosted_zone_changed_files.split(" ")]
+
 def main():
     # # service = Route53Service()
     # # print(service.get_aws_zones())
