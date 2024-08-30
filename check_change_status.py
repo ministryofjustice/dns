4ab46875a2d057c007a8551954ab420639b78443
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2024-08-30 16:37:19 +0100

    firget yml

--- check_change_status.py
+++ check_change_status.py
@@ -6,17 +6,7 @@ from providers.route53 import Route53Service
 hosted_zone_changed_files = os.getenv("hosted_zone_changed_files")
 
 # Strip hostedzones/ from prefix and .yaml from suffix
-# hosted_zone_names = [path[12:-5] for path in hosted_zone_changed_files.split(" ")]
-
-hosted_zone_names = []
-for path in hosted_zone_changed_files:
-    if path[-5:] == ".yaml":
-        hosted_zone_names.append(path[12:-5])
-    elif path[-4:] == ".yml":
-        hosted_zone_names.append(path[12:-4])
-    else:
-        print("not a yml or a yaml!!!")
-
+hosted_zone_names = [path[12:-5] for path in hosted_zone_changed_files.split(" ")]
 
 def main():
     # # service = Route53Service()
