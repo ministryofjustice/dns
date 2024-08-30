04a832813c7630d8b6b23021f9a9bd537e67a797
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2024-08-30 15:52:22 +0100

    set correct mode r for read rather than a for append

--- check_change_status.py
+++ check_change_status.py
@@ -8,9 +8,9 @@ env_file = os.getenv("GITHUB_ENV")
 def main():
     # service = Route53Service()
     # print(service.get_aws_zones())
-    with open(env_file, "a", encoding="utf-8") as f:
+    with open(env_file, "r", encoding="utf-8") as f:
         lines = [line.rstrip() for line in f]
-    
+
     print(lines)
 
 if __name__ == "__main__":
