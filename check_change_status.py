3ff2a87047924c070171351443ae37e0f548d4c9
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2024-08-30 15:58:00 +0100

    print github env content

--- check_change_status.py
+++ check_change_status.py
@@ -9,9 +9,9 @@ def main():
     # service = Route53Service()
     # print(service.get_aws_zones())
     with open(env_file, "r", encoding="utf-8") as f:
-        lines = [line.rstrip() for line in f]
+        content = f.readlines()
 
-    print(lines)
+    print(content)
 
 if __name__ == "__main__":
     main()
